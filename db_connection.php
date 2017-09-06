c3d27dab1f63aedef853cd2c62805a39449af363
Dinesh Gajurel <dinesh.gajurel11@gmail.com>
2017-09-06 22:43:41 +0545

    added new Db_connection clas

---
+++ db_connection.php
@@ -0,0 +1,74 @@
+<?php
+class Db_connection {
+
+
+    /**
+     * db connection instance
+     * @var conn
+    //     */
+     var $_conn=NULL;
+    /**
+     * Success message(Connection OK!)
+     * @var bool|null
+     */
+    var $_success = NULL;
+
+    /**
+     * Error message(Exception/Configuration Error)
+     * @var null|string
+     */
+    var $_error = NULL;
+
+    /**
+     * Connection configuration/constants
+     */
+//    const PASSWORD = "toxic555";
+//    const HOST = "localhost";
+//    const USERNAME = "id2040020_nawinkhatiwada";
+//    const DB = "id2040020_mvp_android";
+//    const TIMEOUT = "10";
+
+    const PASSWORD = "";
+    const HOST = "localhost";
+    const USERNAME = "root";
+    const DB = "mvp_android";
+    const TIMEOUT = "10";
+
+
+    // -------------------------------------------------------------
+
+    /**
+     * constructor.
+     */
+    function __construct()
+    {
+        try {
+            $this->_conn = mysqli_connect(self::HOST, self::USERNAME, self::PASSWORD, self::DB);
+            if (!$this->_conn) {
+                $this->_error = "connection error";
+            }
+            $this->_success = true;
+
+        } catch (Exception $e) {
+            $this->_error = $e->getMessage();
+        }
+    }
+
+    //    -------------------------------------------------------------------
+
+    /**
+     * destructor
+     */
+    function __destruct()
+    {
+        try {
+            if ($this->_conn) {
+                $this->_conn->close();
+            }
+        } catch (Exception $e) {
+            echo "Error:" . $e->getMessage();
+        }
+    }
+
+
+}
