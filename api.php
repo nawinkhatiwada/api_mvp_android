a2f12b3e78adccf78b4a93e9ed290b89444d48a2
nawinkhatiwada <nabin.khatiwada@braindigit.com>
2017-09-06 16:08:11 +0545

    login key value check done

--- api.php
+++ api.php
@@ -22,6 +22,21 @@ if (isset($_GET['add'])) {
 
 if (isset($_GET['login'])) {
     $login_data = (array)json_decode(file_get_contents('php://input'), true);
+
+    $response = null;
+    $username = $login_data["username"];
+    $password = $login_data["password"];
+
+    if ($username == null || $password == null) {
+        $response = array(
+            'statusCode' => -1,
+            'statusMessage' => "Unknown Error Occurred !!!",
+            'response' => null
+        );
+        echo json_encode($response);
+        return;
+    }
+
     $obj = new Mvp_android();
     $res = $obj->login($login_data);
     echo $res;
